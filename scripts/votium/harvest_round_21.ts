e1a7971e63918fddbd6fd49010d1ba0123253444
chiaki644 <92945919+chiaki644@users.noreply.github.com>
2022-06-28 13:29:14 +0800

    votium round 21 (#20)

---
+++ scripts/votium/harvest_round_21.ts
@@ -0,0 +1,51 @@
+/* eslint-disable node/no-missing-import */
+import { BigNumber } from "ethers";
+import { ethers } from "hardhat";
+import { ADDRESS, DECIMALS } from "../config";
+import { Round21Rewards } from "./config";
+
+async function main() {
+  for (const item of Round21Rewards) {
+    const symbol: string = Object.entries(ADDRESS).filter(
+      ([, address]) => address.toLowerCase() === item.token.toLowerCase()
+    )[0][0];
+    console.log(
+      `token[${symbol}], address[${item.token}], amount[${ethers.utils.formatUnits(item.amount, DECIMALS[symbol])}]`
+    );
+  }
+
+  const [deployer] = await ethers.getSigners();
+  const cvxLocker = await ethers.getContractAt(
+    "CLeverCVXLocker",
+    "0x96C68D861aDa016Ed98c30C810879F9df7c64154",
+    deployer
+  );
+
+  const estimate = BigNumber.from(
+    await ethers.provider.call({
+      from: "0x11E91BB6d1334585AA37D8F4fde3932C7960B938",
+      to: cvxLocker.address,
+      data: cvxLocker.interface.encodeFunctionData("harvestVotium", [Round21Rewards, 0]),
+    })
+  );
+  console.log("estimate CVX:", ethers.utils.formatEther(estimate.toString()));
+  const gasEstimate = await ethers.provider.estimateGas({
+    from: deployer.address,
+    to: cvxLocker.address,
+    data: cvxLocker.interface.encodeFunctionData("harvestVotium", [Round21Rewards, 0]),
+  });
+  console.log("gas estimate:", gasEstimate.toString());
+  const tx = await cvxLocker.harvestVotium(Round21Rewards, estimate.mul(9).div(10), {
+    gasLimit: gasEstimate.mul(12).div(10),
+  });
+  console.log("waiting for tx:", tx.hash);
+  const receipt = await tx.wait();
+  console.log("confirmed, gas used:", receipt.gasUsed.toString());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
