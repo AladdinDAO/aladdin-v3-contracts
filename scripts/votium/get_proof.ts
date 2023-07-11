fe85c56758f5e3a8d035d92f675cbdded5247b3b
chiaki.hoshin <chiaki.hoshin@gmail.com>
2023-07-11 10:20:45 +0800

    votium round 48

--- scripts/votium/get_proof.ts
+++ scripts/votium/get_proof.ts
@@ -93,6 +93,7 @@ const REWARDS: { [round: number]: string[] } = {
     "sdFXS",
   ],
   47: ["ALCX", "CLEV", "CNC", "CRV", "CVX", "FXS", "GNO", "INV", "MET", "OGV", "SPELL", "STG", "TUSD", "USDC", "USDD"],
+  48: ["ALCX", "CLEV", "CNC", "CRV", "CVX", "FXS", "GNO", "MET", "OGV", "SPELL", "TUSD", "USDC", "USDD", "sdFXS"],
 };
 
 async function main(round: number) {
