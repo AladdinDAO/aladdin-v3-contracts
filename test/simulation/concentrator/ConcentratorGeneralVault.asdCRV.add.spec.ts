a0e1a84bf6dc77ccbee712333b8c77daed13237d
chiaki.hoshin <chiaki.hoshin@gmail.com>
2023-11-23 11:36:42 +0800

    add 3 reserve pools in asdcrv vault

--- test/simulation/concentrator/ConcentratorGeneralVault.asdCRV.add.spec.ts
+++ test/simulation/concentrator/ConcentratorGeneralVault.asdCRV.add.spec.ts
@@ -1,26 +1,12 @@
 /* eslint-disable camelcase */
-/* eslint-disable node/no-missing-import */
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 import { expect } from "chai";
-import { constants } from "ethers";
+import { MaxUint256, ZeroAddress } from "ethers";
 import { ethers, network } from "hardhat";
-import {
-  ADDRESS,
-  DEPLOYED_CONTRACTS,
-  TOKENS,
-  AVAILABLE_VAULTS,
-  ZAP_ROUTES,
-  DEPLOYED_VAULTS,
-} from "../../../scripts/utils";
-import {
-  AladdinZap,
-  ConcentratorGateway,
-  ConcentratorVaultForAsdCRV,
-  IConvexBooster,
-  IERC20,
-} from "../../../typechain";
-// eslint-disable-next-line camelcase
-import { request_fork } from "../../utils";
+
+import { request_fork } from "@/test/utils";
+import { AladdinZap, ConcentratorGateway, ConcentratorVaultForAsdCRV, IConvexBooster, IERC20 } from "@/types/index";
+import { ADDRESS, AVAILABLE_VAULTS, DEPLOYED_CONTRACTS, DEPLOYED_VAULTS, TOKENS, ZAP_ROUTES } from "@/utils/index";
 
 const strategies: {
   factory: string;
@@ -29,10 +15,10 @@ const strategies: {
   factory: "0x23384DD4380b3677b829C6c88c0Ea9cc41C099bb",
   impls: {
     AutoCompoundingConvexFraxStrategy: "0x6Cc546cE582b0dD106c231181f7782C79Ef401da",
-    AutoCompoundingConvexCurveStrategy: constants.AddressZero,
+    AutoCompoundingConvexCurveStrategy: ZeroAddress,
     ManualCompoundingConvexCurveStrategy: "0xE25f0E29060AeC19a0559A2EF8366a5AF086222e",
     ManualCompoundingCurveGaugeStrategy: "0x188bd82BF11cC321F7872acdCa4B1a3Bf9a802dE",
-    CLeverGaugeStrategy: constants.AddressZero,
+    CLeverGaugeStrategy: ZeroAddress,
     AMOConvexCurveStrategy: "0x2be5B652836C630E15c3530bf642b544ae901239",
   },
 };
@@ -79,6 +65,30 @@ const POOL_FORK_CONFIG: {
     amount: "10",
     harvest: true,
   },
+  "hyUSD/eUSD": {
+    height: 18524680,
+    pid: 13,
+    deployer: "0xDA9dfA130Df4dE4673b89022EE50ff26f6EA73Cf",
+    holder: "0xeCb456EA5365865EbAb8a2661B0c503410e9B347",
+    amount: "100",
+    harvest: true,
+  },
+  "ETH+/ETH": {
+    height: 18524680,
+    pid: 13,
+    deployer: "0xDA9dfA130Df4dE4673b89022EE50ff26f6EA73Cf",
+    holder: "0xeCb456EA5365865EbAb8a2661B0c503410e9B347",
+    amount: "0.1",
+    harvest: true,
+  },
+  "eUSD/FRAXBP": {
+    height: 18524680,
+    pid: 13,
+    deployer: "0xDA9dfA130Df4dE4673b89022EE50ff26f6EA73Cf",
+    holder: "0xf83deAdE1b0D2AfF07700C548a54700a082388bE",
+    amount: "100",
+    harvest: true,
+  },
 };
 
 const BOOSTER = "0xF403C135812408BFbE8713b5A23a04b3D48AAE31";
@@ -86,32 +96,15 @@ const PRINT_ZAP = true;
 const POOLS = (process.env.POOLS || "").split(",");
 
 describe("ConcentratorGeneralVault.asdCRV.add.spec", async () => {
-  let deployer: SignerWithAddress;
-  let signer: SignerWithAddress;
+  let deployer: HardhatEthersSigner;
+  let signer: HardhatEthersSigner;
+
   let lpToken: IERC20;
   let vault: ConcentratorVaultForAsdCRV;
   let zap: AladdinZap;
   let gateway: ConcentratorGateway;
 
   if (PRINT_ZAP) {
-    DEPLOYED_VAULTS.asdCRV.forEach(({ name, fees }) => {
-      const config = AVAILABLE_VAULTS[name];
-      const fork = POOL_FORK_CONFIG[name];
-      if (fork === undefined) {
-        return;
-      }
-      if (!POOLS.includes(name)) return;
-
-      console.log(
-        `add pool[${name}]:`,
-        `convexCurveID[${config.convexCurveID}]`,
-        `gauge[${config.gauge}]`,
-        `rewards[${config.rewards}]`,
-        `withdrawFee[${fees.withdraw}]`,
-        `platformFee[${fees.platform}]`,
-        `harvestBounty[${fees.harvest}]`
-      );
-    });
     console.log("{");
     DEPLOYED_VAULTS.asdCRV.forEach(({ name }) => {
       const config = AVAILABLE_VAULTS[name];
@@ -125,14 +118,14 @@ describe("ConcentratorGeneralVault.asdCRV.add.spec", async () => {
       Object.entries(config.deposit).forEach(([symbol, routes]) => {
         if (symbol === "WETH") {
           console.log(
-            `    {"symbol": "ETH", "address": "${constants.AddressZero}", "routes": [${routes
-              .map((x) => `"${x.toHexString()}"`)
+            `    {"symbol": "ETH", "address": "${ZeroAddress}", "routes": [${routes
+              .map((x) => `"0x${x.toString(16)}"`)
               .join(",")}]},`
           );
         }
         console.log(
           `    {"symbol": "${symbol}", "address": "${TOKENS[symbol].address}", "routes": [${routes
-            .map((x) => `"${x.toHexString()}"`)
+            .map((x) => `"0x${x.toString(16)}"`)
             .join(",")}]},`
         );
       });
@@ -172,43 +165,42 @@ describe("ConcentratorGeneralVault.asdCRV.add.spec", async () => {
         const manager = await ethers.getSigner(DEPLOYED_CONTRACTS.ManagementMultisig);
         const owner = await ethers.getSigner(DEPLOYED_CONTRACTS.Concentrator.Treasury);
 
-        await deployer.sendTransaction({ to: signer.address, value: ethers.utils.parseEther("10") });
-        await deployer.sendTransaction({ to: manager.address, value: ethers.utils.parseEther("10") });
-        await deployer.sendTransaction({ to: owner.address, value: ethers.utils.parseEther("10") });
+        await deployer.sendTransaction({ to: signer.address, value: ethers.parseEther("10") });
+        await deployer.sendTransaction({ to: manager.address, value: ethers.parseEther("10") });
+        await deployer.sendTransaction({ to: owner.address, value: ethers.parseEther("10") });
 
-        lpToken = await ethers.getContractAt("IERC20", ADDRESS[`${config.token}_TOKEN`]);
+        lpToken = await ethers.getContractAt("MockERC20", ADDRESS[`${config.token}_TOKEN`]);
 
         const TokenZapLogic = await ethers.getContractFactory("TokenZapLogic", deployer);
         const logic = await TokenZapLogic.deploy();
-        await logic.deployed();
 
         // upgrade zap contract
         const proxyAdmin = await ethers.getContractAt("ProxyAdmin", DEPLOYED_CONTRACTS.Concentrator.ProxyAdmin, owner);
         const AladdinZap = await ethers.getContractFactory("AladdinZap", deployer);
         const impl = await AladdinZap.deploy();
-        await proxyAdmin.upgrade(DEPLOYED_CONTRACTS.AladdinZap, impl.address);
+        await proxyAdmin.upgrade(DEPLOYED_CONTRACTS.AladdinZap, impl.getAddress());
         zap = await ethers.getContractAt("AladdinZap", DEPLOYED_CONTRACTS.AladdinZap, manager);
 
         // setup withdraw zap
-        await zap.updatePoolTokens([ADDRESS[`${config.token}_POOL`]], [lpToken.address]);
+        await zap.updatePoolTokens([ADDRESS[`${config.token}_POOL`]], [lpToken.getAddress()]);
         if (ADDRESS[`${config.token}_DEPOSIT`]) {
-          await zap.updatePoolTokens([ADDRESS[`${config.token}_DEPOSIT`]], [lpToken.address]);
+          await zap.updatePoolTokens([ADDRESS[`${config.token}_DEPOSIT`]], [lpToken.getAddress()]);
         }
         for (const [symbol, routes] of Object.entries(config.withdraw)) {
-          await zap.updateRoute(lpToken.address, ADDRESS[symbol], routes);
+          await zap.updateRoute(lpToken.getAddress(), ADDRESS[symbol], routes);
         }
 
         gateway = await ethers.getContractAt(
           "ConcentratorGateway",
           DEPLOYED_CONTRACTS.Concentrator.ConcentratorGateway,
-          manager
+          owner
         );
         const gatewayOwner = await ethers.getSigner(await gateway.owner());
-        await gateway.connect(gatewayOwner).updateLogic(logic.address);
+        await gateway.connect(gatewayOwner).updateLogic(logic.getAddress());
 
         const strategyName = `ManualCompounding${strategy}Strategy`;
         const factory = await ethers.getContractAt("ConcentratorStrategyFactory", strategies.factory, deployer);
-        const strategyAddress = await factory.callStatic.createStrategy(strategies.impls[strategyName]);
+        const strategyAddress = await factory.createStrategy.staticCall(strategies.impls[strategyName]);
         await factory.createStrategy(strategies.impls[strategyName]);
         const strategyContract = await ethers.getContractAt(strategyName, strategyAddress, deployer);
         const underlying = ADDRESS[`${config.token}_TOKEN`];
@@ -220,39 +212,39 @@ describe("ConcentratorGeneralVault.asdCRV.add.spec", async () => {
         );
         const vaultOwner = await ethers.getSigner(await vault.owner());
         if (strategy === "ConvexCurve") {
-          await strategyContract.initialize(vault.address, underlying, config.rewarder!, config.rewards);
+          await strategyContract.initialize(vault.getAddress(), underlying, config.rewarder!, config.rewards);
         } else if (strategy === "CurveGauge") {
-          await strategyContract.initialize(vault.address, underlying, config.gauge!, config.rewards);
+          await strategyContract.initialize(vault.getAddress(), underlying, config.gauge!, config.rewards);
         }
         await vault
           .connect(vaultOwner)
           .addPool(underlying, strategyAddress, fees.withdraw, fees.platform, fees.harvest);
-        await vault.connect(vaultOwner).updateHarvester(constants.AddressZero);
+        await vault.connect(vaultOwner).updateHarvester(ZeroAddress);
       });
 
       context("deposit", async () => {
-        const amountLP = ethers.utils.parseEther(fork.amount);
+        const amountLP = ethers.parseEther(fork.amount);
         if (config.deposit.WETH !== undefined) {
           it("deposit, withdraw as ETH, deposit from ETH", async () => {
             // zap to ETH
-            await lpToken.connect(signer).approve(zap.address, amountLP);
-            const etherBefore = await signer.getBalance();
-            const tx = await zap.connect(signer).zapFrom(lpToken.address, amountLP, constants.AddressZero, 0);
+            await lpToken.connect(signer).approve(zap.getAddress(), amountLP);
+            const etherBefore = await ethers.provider.getBalance(signer.address);
+            const tx = await zap.connect(signer).zapFrom(lpToken.getAddress(), amountLP, ZeroAddress, 0);
             const receipt = await tx.wait();
-            const baseFee = (await ethers.provider.getFeeData()).lastBaseFeePerGas!;
-            const effectiveGasPrice = tx.gasPrice ? tx.gasPrice : baseFee.add(tx.maxPriorityFeePerGas!);
-            const etherAfter = await signer.getBalance();
-            expect(etherAfter.add(receipt.gasUsed.mul(effectiveGasPrice))).gt(etherBefore);
+            const baseFee = (await ethers.provider.getBlock("latest"))?.baseFeePerGas!;
+            const effectiveGasPrice = tx.gasPrice ? tx.gasPrice : baseFee + tx.maxPriorityFeePerGas!;
+            const etherAfter = await ethers.provider.getBalance(signer.address);
+            expect(etherAfter + receipt!.gasUsed * effectiveGasPrice).gt(etherBefore);
 
             // zap from ETH
-            const amountIn = etherAfter.add(receipt.gasUsed.mul(effectiveGasPrice)).sub(etherBefore);
+            const amountIn = etherAfter + receipt!.gasUsed * effectiveGasPrice - etherBefore;
             await gateway
               .connect(signer)
               .deposit(
-                vault.address,
+                vault.getAddress(),
                 fork.pid,
-                constants.AddressZero,
-                lpToken.address,
+                ZeroAddress,
+                lpToken.getAddress(),
                 amountIn,
                 config.deposit.WETH,
                 0,
@@ -262,46 +254,45 @@ describe("ConcentratorGeneralVault.asdCRV.add.spec", async () => {
               );
             const zapSharesOut = await vault.getUserShare(fork.pid, signer.address);
             console.log(
-              `amountLP[${ethers.utils.formatEther(amountLP)}]`,
-              `amountIn[${ethers.utils.formatEther(amountIn)}]`,
-              `zapSharesOut[${ethers.utils.formatEther(zapSharesOut)}]`
+              `amountLP[${ethers.formatEther(amountLP)}]`,
+              `amountIn[${ethers.formatEther(amountIn)}]`,
+              `zapSharesOut[${ethers.formatEther(zapSharesOut)}]`
             );
-            expect(zapSharesOut).to.gt(constants.Zero);
-            expect(zapSharesOut).to.closeToBn(amountLP, amountLP.mul(2).div(100)); // 2% error
+            expect(zapSharesOut).to.gt(0n);
+            expect(zapSharesOut).to.closeTo(amountLP, (amountLP * 2n) / 100n); // 2% error
           });
         }
 
         Object.entries(config.deposit).forEach(([symbol, routes]) => {
           it(`deposit, withdraw as ${symbol}, deposit from ${symbol}`, async () => {
-            const token = await ethers.getContractAt("IERC20", ADDRESS[symbol], signer);
+            const token = await ethers.getContractAt("MockERC20", ADDRESS[symbol], signer);
             // zap to token
-            await lpToken.connect(signer).approve(zap.address, amountLP);
+            await lpToken.connect(signer).approve(zap.getAddress(), amountLP);
             const tokenBefore = await token.balanceOf(signer.address);
-            await zap.connect(signer).zapFrom(lpToken.address, amountLP, token.address, 0);
+            await zap.connect(signer).zapFrom(lpToken.getAddress(), amountLP, token.getAddress(), 0);
             const tokenAfter = await token.balanceOf(signer.address);
-            expect(tokenAfter.gt(tokenBefore));
+            expect(tokenAfter).to.gt(tokenBefore);
             // zap from token
-            const amountIn = tokenAfter.sub(tokenBefore);
-            await token.approve(gateway.address, constants.Zero);
-            await token.approve(gateway.address, constants.MaxUint256);
+            const amountIn = tokenAfter - tokenBefore;
+            await token.approve(gateway.getAddress(), MaxUint256);
             await gateway
               .connect(signer)
-              .deposit(vault.address, fork.pid, token.address, lpToken.address, amountIn, routes, 0);
+              .deposit(vault.getAddress(), fork.pid, token.getAddress(), lpToken.getAddress(), amountIn, routes, 0);
             const zapSharesOut = await vault.getUserShare(fork.pid, signer.address);
             console.log(
-              `amountLP[${ethers.utils.formatEther(amountLP)}]`,
-              `amountIn[${ethers.utils.formatUnits(amountIn, TOKENS[symbol].decimals)}]`,
-              `zapSharesOut[${ethers.utils.formatEther(zapSharesOut)}]`
+              `amountLP[${ethers.formatEther(amountLP)}]`,
+              `amountIn[${ethers.formatUnits(amountIn, TOKENS[symbol].decimals)}]`,
+              `zapSharesOut[${ethers.formatEther(zapSharesOut)}]`
             );
-            expect(zapSharesOut).to.gt(constants.Zero);
-            expect(zapSharesOut).to.closeToBn(amountLP, amountLP.mul(2).div(100)); // 2% error
+            expect(zapSharesOut).to.gt(0n);
+            expect(zapSharesOut).to.closeTo(amountLP, (amountLP * 2n) / 100n); // 2% error
           });
         });
       });
 
       if (fork.harvest) {
         context("harvest", async () => {
-          const amountLP = ethers.utils.parseEther(fork.amount);
+          const amountLP = ethers.parseEther(fork.amount);
           let booster: IConvexBooster;
           let firstCall = true;
 
@@ -323,7 +314,7 @@ describe("ConcentratorGeneralVault.asdCRV.add.spec", async () => {
             }
             firstCall = false;
 
-            await lpToken.connect(signer).approve(vault.address, amountLP);
+            await lpToken.connect(signer).approve(vault.getAddress(), amountLP);
             await vault.connect(signer).deposit(fork.pid, signer.address, amountLP);
             const sharesOut = await vault.getUserShare(fork.pid, signer.address);
             expect(sharesOut).to.eq(amountLP);
@@ -333,26 +324,21 @@ describe("ConcentratorGeneralVault.asdCRV.add.spec", async () => {
             if (config.convexCurveID) {
               await booster.earmarkRewards(config.convexCurveID);
             }
-            const timestamp = (await ethers.provider.getBlock("latest")).timestamp;
+            const timestamp = (await ethers.provider.getBlock("latest"))!.timestamp;
             await network.provider.send("evm_setNextBlockTimestamp", [timestamp + 86400 * 7]);
             await network.provider.send("evm_mine");
 
             const token = await ethers.getContractAt(
-              "IERC20",
+              "MockERC20",
               DEPLOYED_CONTRACTS.Concentrator.StakeDAO.sdCRV.asdCRV,
               deployer
             );
-            const amount = await vault.callStatic.harvest(fork.pid, deployer.address, 0);
-            const before = await token.balanceOf(vault.address);
+            const amount = await vault.harvest.staticCall(fork.pid, deployer.address, 0);
+            const before = await token.balanceOf(vault.getAddress());
             await vault.harvest(fork.pid, deployer.address, 0);
-            const after = await token.balanceOf(vault.address);
-            console.log(
-              "harvested sdCRV:",
-              ethers.utils.formatEther(amount),
-              "asdCRV:",
-              ethers.utils.formatEther(after.sub(before))
-            );
-            expect(amount).gt(constants.Zero);
+            const after = await token.balanceOf(vault.getAddress());
+            console.log("harvested sdCRV:", ethers.formatEther(amount), "asdCRV:", ethers.formatEther(after - before));
+            expect(amount).gt(0n);
           });
         });
       }
