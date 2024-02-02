df50c6468ff8c0619d4ef939649903bfae39a0ef
chiaki.hoshin <chiaki.hoshin@gmail.com>
2024-02-02 13:10:07 +0800

    update clever deploy script

--- scripts/contracts/CLeverCVX.ts
+++ scripts/contracts/CLeverCVX.ts
@@ -1,14 +1,25 @@
+/* eslint-disable camelcase */
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
-import { network } from "hardhat";
+import { Overrides } from "ethers";
+import { ethers, network } from "hardhat";
 
-import { selectDeployments } from "@/utils/deploys";
+import { CLeverCVXLocker__factory, Furnace__factory } from "@/types/index";
+import { selectDeployments } from "@/utils/index";
 
-import { contractDeploy } from "./helpers";
+import { DeploymentHelper, upgradeCall } from "./helpers";
+import * as Multisig from "./Multisig";
+import * as ProxyAdmin from "./ProxyAdmin";
 
 export interface CLeverCVXDeployment {
   clevCVX: string;
-  CVXLocker: string;
-  Furnace: string;
+  CVXLocker: {
+    proxy: string;
+    implementation: string;
+  };
+  Furnace: {
+    proxy: string;
+    implementation: string;
+  };
 }
 
 export interface CLeverCVXHermezDeployment {
@@ -17,23 +28,77 @@ export interface CLeverCVXHermezDeployment {
 
 const CONVEX_VOTE_PLATFORM = "0x6D024Fa49dE64A975980Cddd4C3212492D189e57";
 
-export async function deploy(deployer: HardhatEthersSigner): Promise<CLeverCVXDeployment> {
-  const deployment = selectDeployments(network.name, "CLever.CVX");
+export async function deploy(deployer: HardhatEthersSigner, overrides?: Overrides): Promise<CLeverCVXDeployment> {
+  const multisig = Multisig.deploy(network.name);
+  const admin = await ProxyAdmin.deploy(deployer);
+  const deployment = new DeploymentHelper(network.name, "CLever.CVX", deployer, overrides);
+
+  await deployment.contractDeploy("clevCVX", "clevCVX", "CLeverToken", ["CLever CVX", "clevCVX"]);
+  await deployment.contractDeploy("Furnace.implementation", "Furnace implementation", "Furnace", []);
+  await deployment.contractDeploy("CVXLocker.implementation", "CVXLocker implementation", "CLeverCVXLocker", []);
+
+  await deployment.proxyDeploy(
+    "Furnace.proxy",
+    "Furnace proxy",
+    deployment.get("Furnace.implementation"),
+    admin.CLever,
+    Furnace__factory.createInterface().encodeFunctionData("initialize", [
+      multisig.AladdinDAO,
+      deployment.get("clevCVX"),
+      "0x1104b4DF568fa7Af90B1Bed1D78A2F71e748dc8a",
+      multisig.CLever,
+      20000000n,
+      10000000n,
+    ])
+  );
+  await deployment.proxyDeploy(
+    "CVXLocker.proxy",
+    "CVXLocker proxy",
+    deployment.get("CVXLocker.implementation"),
+    admin.CLever,
+    CLeverCVXLocker__factory.createInterface().encodeFunctionData("initialize", [
+      multisig.AladdinDAO,
+      deployment.get("clevCVX"),
+      "0x1104b4DF568fa7Af90B1Bed1D78A2F71e748dc8a",
+      deployment.get("Furnace.proxy"),
+      multisig.CLever,
+      200000000n,
+      10000000n,
+    ])
+  );
 
   return deployment.toObject() as CLeverCVXDeployment;
 }
 
-export async function deployVoter(deployer: HardhatEthersSigner): Promise<CLeverCVXHermezDeployment> {
-  const clevcvx = selectDeployments("mainnet", "CLever.CVX");
-  const deployment = selectDeployments("hermez", "vlCVX.Voter");
+export async function initialize(
+  deployer: HardhatEthersSigner,
+  deployment: CLeverCVXDeployment,
+  overrides?: Overrides
+) {
+  const admin = await ProxyAdmin.deploy(deployer);
+  const proxyAdmin = await ethers.getContractAt("ProxyAdmin", admin.CLever, deployer);
 
-  if (!deployment.get("ConvexHermezVoter")) {
-    const address = await contractDeploy(deployer, "ConvexHermezVoter", "ConvexHermezVoter", [
-      clevcvx.get("CVXLocker"),
-      CONVEX_VOTE_PLATFORM,
-    ]);
-    deployment.set("ConvexHermezVoter", address);
+  /* eslint-disable prettier/prettier */
+  // prettier-ignore
+  // eslint-disable-next-line no-lone-blocks
+  {
+  await upgradeCall(proxyAdmin, "CLeverCVXLocker", deployment.CVXLocker.proxy, deployment.CVXLocker.implementation, overrides);
+  await upgradeCall(proxyAdmin, "Furnace", deployment.Furnace.proxy, deployment.Furnace.implementation, overrides);
   }
+  /* eslint-enable prettier/prettier */
+}
+
+export async function deployVoter(
+  deployer: HardhatEthersSigner,
+  overrides?: Overrides
+): Promise<CLeverCVXHermezDeployment> {
+  const clevcvx = selectDeployments("mainnet", "CLever.CVX");
+  const deployment = new DeploymentHelper(network.name, "vlCVX.Voter", deployer, overrides);
+
+  await deployment.contractDeploy("ConvexHermezVoter", "ConvexHermezVoter", "ConvexHermezVoter", [
+    clevcvx.get("CVXLocker"),
+    CONVEX_VOTE_PLATFORM,
+  ]);
 
   return deployment.toObject() as CLeverCVXHermezDeployment;
 }
